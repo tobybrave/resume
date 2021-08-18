4bbefe274dc8ff7cf542e810736edb19bd8d70e8
tobybrave <leutoby@gmail.com>
2021-08-18 12:19:33 +0100

    stringified env variables

--- services/mailer.js
+++ services/mailer.js
@@ -3,17 +3,17 @@ const { SMTP_HOST, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD, CLIENT } = process.e
 
 const sendMail = async ({ name, email, phone, work, purpose, description }) => {
   let transporter = nodemailer.createTransport({
-    host: SMTP_HOST,
+    host: `${SMTP_HOST}`,
     port: parseInt(SMTP_PORT),
     auth: {
-      user: SMTP_USERNAME,
-      pass: SMTP_PASSWORD
+      user: `${SMTP_USERNAME}`,
+      pass: `${SMTP_PASSWORD}`
     }
   })
 
   let info = await transporter.sendMail({
     from: `${name} <${email}>`,
-    to: CLIENT,
+    to: `${CLIENT}`,
     subject: `${purpose}`,
     text: `I'm ${name} from ${work}. ${description}. ${phone ? `You can reach me on phone ${phone}` : ""}`,
     html: `<b>I'm ${name} from ${work}. ${description}. ${phone ? `You can reach me on phone ${phone}` : ""}</b>`
