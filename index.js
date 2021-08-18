109e6f6b41dd5b899d5918ac174a1f92889cb9b7
tobybrave <leutoby@gmail.com>
2021-08-18 13:40:51 +0100

    linting

--- index.js
+++ index.js
@@ -7,30 +7,29 @@ const PORT = process.env.PORT
 
 const server = http.createServer((request, response) => {
   try {
-  if (request.url.match(/.css$/) || request.url.match(/.js$/)) {
-    const file = path.join(__dirname, "ui", request.url)
+    if (request.url.match(/.css$/) || request.url.match(/.js$/)) {
+      const file = path.join(__dirname, "ui", request.url)
     
-    fs.createReadStream(file, "utf-8")
-      .pipe(response)
-  }
-  
-	  else if (request.method === "POST" && request.url === "/sendMessage") {
-    request.on("data", async (data) => {
-      const details = JSON.parse(data.toString())
-      await sendMail(details).catch(console.error)
-    })
-  }
-  else { 
-  fs.readFile(`${__dirname}/ui/index.html`, "utf-8", (error, data) => {
-    if (error) {
-      response.writeHead(500)
-      return response.end("File not found")
+      fs.createReadStream(file, "utf-8")
+        .pipe(response)
     }
+    else if (request.method === "POST" && request.url === "/sendMessage") {
+      request.on("data", async (data) => {
+        const details = JSON.parse(data.toString())
+        return await sendMail(details).catch(console.error)
+      })
+    }
+    else { 
+      fs.readFile(`${__dirname}/ui/index.html`, "utf-8", (error, data) => {
+        if (error) {
+          response.writeHead(500)
+          return response.end("File not found")
+        }
     
-    response.writeHead(200, { "Content-Type": "text/html"})
-    response.end(data)
-  })
-  }
+        response.writeHead(200, { "Content-Type": "text/html"})
+        response.end(data)
+      })
+    }
   } catch (error) {
     console.error(error)
   }
--- services/mailer.js
+++ services/mailer.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config()
 const nodemailer = require("nodemailer")
 const { SMTP_HOST, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD, CLIENT } = process.env
 
